ed3ad5c19181de3cb48da0e9a80501c519f34273
Woomin Ko <woomin422@gmail.com>
2025-01-30 19:08:37 +0900

    done with connecting server

--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,255 @@
-import 'package:http/http.dart' as http;
+import 'dart:async';
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
+Future<Album> createAlbum(String title, String content) async {
+  final response = await http.post(
+    Uri.parse('http://127.0.0.1:8080/api/articles'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'title': title,
+      'content':content,
+    }),
+  );
 
-
-Future<Album> fetchLogin() async {
-  final response = await http
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
+  if (response.statusCode == 201) {
+    // If the server did return a 201 CREATED response,
     // then parse the JSON.
     return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
   } else {
-    // If the server did not return a 200 OK response,
+    // If the server did not return a 201 CREATED response,
     // then throw an exception.
-    throw Exception('Failed to load album');
+    throw Exception('Failed to create album.');
   }
 }
-class Album {
-  final int userId;
-  final int id;
-  final String title;
 
-  const Album({
-     required this.userId,
-    required this.id,
-    required this.title,
-  });
+class Album {
+  final String title;
+  final String content;
+
+  const Album({ required this.title, required this.content});
 
   factory Album.fromJson(Map<String, dynamic> json) {
     return switch (json) {
       {
-       'userId': int userId,
-        'id': int id,
         'title': String title,
+        'content':String content,
       } =>
         Album(
-          userId: userId,
-          id: id,
           title: title,
+          content: content,
         ),
       _ => throw const FormatException('Failed to load album.'),
     };
   }
 }
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _controller = TextEditingController();
+  final TextEditingController _controller2 = TextEditingController();
+  Future<Album>? _futureAlbum;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: fdfdf(),
-      routes: {
-        'second': (context) => const SecondPage(),
-      },
-    );
-    
-  }
-}
-
-class fdfdf extends StatefulWidget {
-  const fdfdf({super.key});
-
-  @override
-  State<fdfdf> createState() => _fdfdfState();
-}
-
-class _fdfdfState extends State<fdfdf> {
-  
-
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [IconButton(onPressed: (){
-          Navigator.of(context).pushNamed('second');
-        } , icon:Icon(Icons.login))],
+      title: 'Create Data Example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Create Data Example'),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(8),
+          child: (_futureAlbum == null) ? buildColumn() : buildFutureBuilder(),
+        ),
       ),
     );
-    
   }
-}
 
-class SecondPage extends StatefulWidget {
-  const SecondPage({super.key});
-
-  @override
-  State<SecondPage> createState() => _SecondPageState();
-}
-
-class _SecondPageState extends State<SecondPage> {
-  @override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchLogin();
-  }
-  late Future<Album> futureAlbum;
-  @override
-  Widget build(BuildContext context) {
+  Column buildColumn() {
     return Column(
-      children: [FutureBuilder<Album>(
-  future: futureAlbum,
-  builder: (context, snapshot) {
-    if (snapshot.hasData) {
-      return Text(snapshot.data!.title);
-    } else if (snapshot.hasError) {
-      return Text('${snapshot.error}');
-    }
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextField(
+          controller: _controller,
+          decoration: const InputDecoration(hintText: 'Enter Title'),
+        ),
+        TextField(
+          controller: _controller2,
+          decoration: const InputDecoration(hintText: 'Enter Content'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _futureAlbum = createAlbum(_controller.text,_controller2.text);
+            });
+          },
+          child: const Text('Create Data'),
+        ),
+      ],
+    );
+  }
 
-    // By default, show a loading spinner.
-    return const CircularProgressIndicator();
-  },
-)])
-    
-;
+  FutureBuilder<Album> buildFutureBuilder() {
+    return FutureBuilder<Album>(
+      future: _futureAlbum,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text(snapshot.data!.title);
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        return const CircularProgressIndicator();
+      },
+    );
   }
 }
+
+
+
+// import 'dart:ffi';
+
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+
+
+
+// Future<Album> createAlbum(String title) async {
+//   final response = await http.post(
+//     Uri.parse('https://jsonplaceholder.typicode.com/albums'),
+//     headers: <String, String>{
+//       'Content-Type': 'application/json; charset=UTF-8',
+//     },
+//     body: jsonEncode(<String, String>{
+//       'title': title,
+//     }),
+//   );
+
+//   if (response.statusCode == 201) {
+//     // If the server did return a 201 CREATED response,
+//     // then parse the JSON.
+//     return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+//   } else {
+//     // If the server did not return a 201 CREATED response,
+//     // then throw an exception.
+//     throw Exception('Failed to create album.');
+//   }
+// }
+// class Album {
+//   final int id;
+//   final String title;
+
+//   const Album({required this.id, required this.title});
+
+//   factory Album.fromJson(Map<String, dynamic> json) {
+//     return switch (json) {
+//       {
+//         'id': int id,
+//         'title': String title,
+//       } =>
+//         Album(
+//           id: id,
+//           title: title,
+//         ),
+//       _ => throw const FormatException('Failed to load album.'),
+//     };
+//   }
+// }
+// void main() {
+//   runApp(MyApp());
+// }
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: fdfdf(),
+//       routes: {
+//         'second': (context) => const SecondPage(),
+//       },
+//     );
+    
+//   }
+// }
+
+// class fdfdf extends StatefulWidget {
+//   const fdfdf({super.key});
+
+//   @override
+//   State<fdfdf> createState() => _fdfdfState();
+// }
+
+// class _fdfdfState extends State<fdfdf> {
+  
+
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         children: [IconButton(onPressed: (){
+//           Navigator.of(context).pushNamed('second');
+//         } , icon:Icon(Icons.login))],
+//       ),
+//     );
+    
+//   }
+// }
+
+// class SecondPage extends StatefulWidget {
+//   const SecondPage({super.key});
+
+//   @override
+//   State<SecondPage> createState() => _SecondPageState();
+// }
+
+// class _SecondPageState extends State<SecondPage> {
+
+//   final TextEditingController _controller = TextEditingController();
+//   late Future<Album> _futureAlbum;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//   children: <Widget>[
+//     TextField(
+//       controller: _controller,
+//       decoration: const InputDecoration(hintText: 'Enter Title'),
+//     ),
+//     ElevatedButton(
+//       onPressed: () {
+//         setState(() {
+//           _futureAlbum = createAlbum(_controller.text);
+//         });
+//       },
+//       child: const Text('Create Data'),
+//     ),
+//   ],
+//       )
+    
+// ;
+//   }
+// }
